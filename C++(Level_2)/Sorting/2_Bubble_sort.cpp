4ed6dfd11ee66f0a49f3b41e2bf22ccae06642cb
deepanshu-cmd <deepanshu97531@gmail.com>
2024-08-10 20:23:11 +0530

    Modified bubble sort file  in sorting folder

--- C++(Level_2)/Sorting/2_Bubble_sort.cpp
+++ C++(Level_2)/Sorting/2_Bubble_sort.cpp
@@ -2,19 +2,54 @@
 #include <iomanip>
 using namespace std;
 
+//* Bubble sort is a simple sorting algorithm that repeatedly steps through the list, compares adjacent elements, and swaps them if they are in the wrong order. The pass through the list is repeated until the list is sorted.
 void bubble_sort(int arr[], int arr_length)
 {
-    //* Bubble sort is a simple sorting algorithm that repeatedly steps through the list, compares adjacent elements, and swaps them if they are in the wrong order. The pass through the list is repeated until the list is sorted.
-    for (int i = 0; i < arr_length; i++)
+    //% Sort in increasing order:
+    for (int i = 1; i < arr_length; i++)
     {
+        bool swapped = false;
         for (int j = 0; j < arr_length - i; j++)
         {
             if (arr[j] > arr[j + 1])
             {
                 swap(arr[j], arr[j + 1]);
             }
+            swapped = true;
         }
-        cout << arr[i] << ' ';
+        if (swapped == false)
+        {
+            break;
+        }
+    }
+    cout << "Array sorted in increasing order: ";
+    for (int i = 0; i < arr_length; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    //% Sort in decreasing order:
+    for (int i = 1; i < arr_length; i++)
+    {
+        bool swapped = false;
+        for (int j = 0; j < arr_length - i; j++)
+        {
+            if (arr[j] < arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+            swapped = true;
+        }
+        if (swapped == false)
+        {
+            break;
+        }
+    }
+    cout << "Array sorted in decreasing order: ";
+    for (int i = 0; i < arr_length; i++)
+    {
+        cout << arr[i] << " ";
     }
 }
 
